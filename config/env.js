35b50af15f7c376b9122c59377e63dd3fb99b8f9
Zach Shaw <zachshaw1@gmail.com>
2018-02-02 12:25:58 +0000

    adding api config

--- config/env.js
+++ config/env.js
@@ -1,6 +1,6 @@
 // Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
 // injected into the application via DefinePlugin in Webpack configuration.
-
+var API = require('./api.js');
 var REACT_APP = /^REACT_APP_/i;
 
 function getClientEnvironment(publicUrl) {
@@ -20,7 +20,8 @@ function getClientEnvironment(publicUrl) {
       // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
       // This should only be used as an escape hatch. Normally you would put
       // images into the `src` and `import` them in code to get their paths.
-      'PUBLIC_URL': JSON.stringify(publicUrl)
+      'PUBLIC_URL': JSON.stringify(publicUrl),
+      'SKIDDLE_API_KEY': JSON.stringify(API.SKIDDLE_API_KEY),
     });
   return {'process.env': processEnv};
 }
--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -8,7 +8,6 @@ var getClientEnvironment = require('./env');
 var paths = require('./paths');
 
 
-
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
 var publicPath = '/';
