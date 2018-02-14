09cf87331fe6c9eb48e257a40ae3c7f54a3ac6e8
Zach Shaw <zachshaw1@gmail.com>
2018-02-14 11:31:58 +0000

    adding escrow contract logic for locking funds until buyer confirms ticket works

--- contracts/TicketExchange.sol
+++ contracts/TicketExchange.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.4.11;
 import './zeppelin/lifecycle/Killable.sol';
 
 contract TicketExchange is Killable {
+  
+  // State variables
+  mapping(uint => Ticket) public tickets;
+  uint ticketCounter;
+  enum TicketStatus { Created, Locked, Inactive }
+  TicketStatus public state;
+
   struct Ticket {
     uint id;
     address seller;
@@ -10,13 +17,9 @@ contract TicketExchange is Killable {
     string eventId;
     string eventName;
     uint256 price;
+    TicketStatus status;
   }
 
-  // State variables
-  mapping(uint => Ticket) public tickets;
-  uint ticketCounter;
-
-  // Events
   event SellTicketEvent (
     uint indexed _id,
     address indexed _seller,
@@ -32,8 +35,9 @@ contract TicketExchange is Killable {
     uint256 _price
   );
 
+  event TicketConfirmed();
+
   function sellTicket(string _eventId, string _eventName, uint _price) public {
-    // New ticket
     ticketCounter++;
 
     // Storing ticket
@@ -43,47 +47,55 @@ contract TicketExchange is Killable {
       0x0,
       _eventId,
       _eventName,
-      _price
+      _price,
+      TicketStatus.Created
     );
 
-    // Fire SellTicket event
     SellTicketEvent(ticketCounter, msg.sender, _eventName, _price);
   }
 
-  function buyTicket(uint _id) payable public {
-    // At least one ticket for sale
+  function buyTicket(uint _id) 
+    payable
+    public
+  {
     require(ticketCounter > 0);
-    // Ticket actually exists (Will never be zero or over ticketCounters value)
     require(_id > 0 && _id <= ticketCounter);
-    
-    // Ticket exists - Fetch from array
+
     Ticket storage ticket = tickets[_id];
 
-    // Ensure ticket hasn't already been bought
     require(ticket.buyer == 0x0);
-    // Ensure ticket buyer isn't the same as the seller
     require(ticket.seller != msg.sender);
-    // Ensure buyers value matches price in Ether
     require(ticket.price == msg.value);
-    // Store buyers address
+
     ticket.buyer = msg.sender;
+    // ticket.seller.transfer(msg.value);
+    ticket.status = TicketStatus.Locked;
 
-    // All checks passed and ticket updated with buyers details so transfer value
-    ticket.seller.transfer(msg.value);
-
-    // Trigger buy event
     BuyTicketEvent(_id, ticket.seller, ticket.buyer, ticket.eventName, ticket.price);
+  }
+
+  function confirmTicket(uint _id)
+    public
+  {
+    Ticket storage ticket = tickets[_id];
+
+    require(ticket.status == TicketStatus.Locked);
+    require(ticket.buyer == msg.sender);
+
+    ticket.status = TicketStatus.Inactive;
+
+    ticket.seller.transfer(this.balance);
+
+    TicketConfirmed();
   }
 
   function getNumberOfTickets() public constant returns (uint) {
     return ticketCounter;
   }
 
-  // Get all tickets for sale returning an array of ticketIds
   function getTicketsForSale() public constant returns (uint[]) {
-    // Check at least one ticket for sale
+
     if (ticketCounter == 0) {
-      // Returns array of length zero so frontend can handle this
       return new uint[](0);
     }
 
@@ -99,7 +111,7 @@ contract TicketExchange is Killable {
       }
     }
 
-    // Create new array just for tickets that have not been sold yet
+    // New array just for tickets that have not been sold yet
     uint[] memory ticketsForSale = new uint[](numberOfTicketsForSale);
 
     for (uint j = 0; j < numberOfTicketsForSale; j++) {
